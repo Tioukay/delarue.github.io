ffe678230a5c189e6440f121a883a68310c319df
Tony Delarue <tdelarue@enseirb-matmeca.fr>
2018-04-23 08:34:03 +0200

    Final commit

---
+++ database/request.sql
@@ -0,0 +1,34 @@
+-- Requests --
+drop view RECAP;
+
+-- When did the user come in January --
+select fk_user, time
+from psh_raw
+where MONTH(time) = '01'
+and direct = 1;
+
+-- When did the user leave in January --
+select fk_user, time
+from psh_raw
+where MONTH(time) = '01'
+and direct = 0;
+
+-- Give every day arrival and departure, and how much time was spent
+CREATE VIEW RECAP as
+select distinct fk_user, ARRI.time as ENTER, DEP.time as QUIT, TIMEDIFF(DEP.time, ARRI.time) as TIME_SPENT
+from psh_raw, (select fk_user as ID_USR, time
+            from psh_raw
+            where direct = 1) ARRI,
+            (select fk_user as ID_USR, time
+            from psh_raw
+            where direct = 0) DEP
+where DAY(ARRI.time) = DAY(DEP.time)
+and fk_user = ARRI.ID_USR
+and fk_user = DEP.ID_USR;
+
+select * from RECAP;
+
+select fk_user, SUM(HOUR(TIME_SPENT)) as HOURS_OF_WORK
+from RECAP
+where MONTH(ENTER) = '01'
+group by fk_user;
